63ff859c1c8537e057d4b1af817ed1e31007a605
Bonikooooo <144252842+Bonikooooo@users.noreply.github.com>
2023-12-05 19:43:10 +0800

    Delete ICT 600 GONZALES 2ND SEM FINALS directory

--- ICT 600 GONZALES 2ND SEM FINALS/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = 'localhost';
-$username = 'root';
-$password = 'root';
-$port = '3307';
-$database = 'jwayne';
-
-$conn = mysqli_connect ($servername, $username, $password, $database, $port);
